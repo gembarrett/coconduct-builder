14dc89e74c945d6c8a9973c3d0e5c17bb50b09e8
gembarrett <gem@gembarrett.com>
2015-10-20 01:53:41 -0400

    added js minification and scripted first question

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,17 +7,32 @@ module.exports = function (grunt) {
         }
       }
     },
+    uglify: {
+      compress: {
+        files: {
+          'dest/main.min.js': ['src/*.js']
+        },
+        options: {
+          mangle: false
+        }
+      },
+    },
     watch: {
       styling: {
         files: ['src/*.scss'],
         tasks: 'sass'
+      },
+      script: {
+        files: ['src/*.js'],
+        tasks: 'uglify'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.registerTask('default', ['sass', 'uglify', 'watch']);
 
 };
---
+++ src/intro.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var onlineSpace = false;
+  var offlineSpace = false;
+  $('#intro button').click(function(){
+    $clickedID = $(this).attr('id');
+    $(this).toggleClass('selected');
+    if ($clickedID == 'onlineSpace') {
+      if (onlineSpace) {
+        onlineSpace = false;
+      } else {
+        onlineSpace = true;
+      }
+    } else {
+      if (offlineSpace) {
+        offlineSpace = false;
+      } else {
+        offlineSpace = true;
+      }
+    }
+  });
+});
