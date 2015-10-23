92ee5337b85549cf7acc5b3382bad924078e8665
gembarrett <gem@gembarrett.com>
2015-10-23 05:12:13 -0400

    trying to disable/enable input field based on whether radio is selected

--- src/people.js
+++ src/people.js
@@ -1,8 +1,29 @@
-// if ($('#yesAdditional').prop('checked', true)) {
-//   if ($('#otherAdditional').val('').change()){
-//     $('#next').prop('disabled',false);
-//     console.log($('#next'));
+// disable text field of Yes radio button when no is selected
+// when yes is selected, enable the text field
+// copy content entered into that text field over to the span in the preview
+// if no is selected again, hide the span in the preview and disable the text field again
+
+
+// $('#otherAdditional').val('').change(function(){
+//   console.log('stuff');
+// });
+
+// while ($('#yesAdditional').prop('checked',false)) {
+//   $('#otherAdditional').prop('disabled', true);
+// }
+
+// var $copyAddPeepsTo = $('#people .output #additional');
+// var $copyAddPeeps = $('#otherAdditional');
+//
+// $($copyAddPeeps).keyup(function() {
+//   $copyAddPeepsTo.text(this.value);
+// });
+
 //   };
 // } else if ($('#noAdditional').prop('checked', true)) {
 //   $('#next').prop('disabled',false);
+// }
+
+// function disableTextField() {
+//
 // }
