16eac5700111179d4248570439e3f5fb85336c53
gembarrett <gem@gembarrett.com>
2015-10-22 23:35:25 -0400

    As per issue #3, preview of code of conduct with input details is now available

--- src/basic.js
+++ src/basic.js
@@ -1,11 +1,22 @@
-$('#communityName, #userName, #communityurl').val('').change(function(){
+$('#communityName, #userName, #communityurl, #coconducturl').val('').change(function(){
   $('#next').prop('disabled',false);
 });
 function getBasicInfo() {
   communityName = $('#communityName').val();
   userName = $('#userName').val();
   communityUrl = $('#communityurl').val();
-  if ($('#coconducturl').val() != '') {
-    coconductUrl = $('#coconducturl').val();
-  }
+  coconductUrl = $('#coconducturl').val();
 }
+var $copyNameTo = $('#basic .output .communityName');
+var $copyName = $('#communityName');
+
+$($copyName).keyup(function() {
+  $copyNameTo.text(this.value);
+});
+
+var $copyUrlTo = $('#basic .output .coconductURL');
+var $copyUrl = $('#coconducturl');
+
+$($copyUrl).keyup(function() {
+  $copyUrlTo.text(this.value);
+});
--- src/template.js
+++ src/template.js
@@ -4,3 +4,4 @@
 //
 // // medium version
 // var mediumVersion = communityName + ' is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age or religion ' + additionalConcerns + '. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any ' + eventType + ' venue, including talks. Conference participants violating these rules may be sanctioned or expelled from the ' + eventType + ' [without a refund] at the discretion of the conference organizers. Our anti-harassment policy can be found at: ' + codeOfConductUrl;
+
