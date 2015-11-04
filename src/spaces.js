a22d8139cd43f2752f656a4f3006b81f3ff8aa1e
gembarrett <gem@gembarrett.com>
2015-11-04 17:27:31 +0000

    machine hasn't exploded yet. Continuing with spaces section. Damn, this is hard to do when tired.

--- src/spaces.js
+++ src/spaces.js
@@ -1,72 +1,82 @@
 var spaceSelections = $('#spaces [type=checkbox]');
 
-var prefixSpaces = 'including: ' + '<ul>';
+var prefixSpacesTo = $('#prefixing');
 var copyOtherSpacesTo = $('#commSpaces');
+var otherSpacesTo = $('#otherSpaces');
 var copyOtherSpacesFrom = $('#otherSpaceText');
 var otherCheckbox = $('#otherSpace');
+var onlineChoices = $('#spaces #onlineSpaces input[type=checkbox]');
+var offlineChoices = $('#spaces #offlineSpaces input[type=checkbox]')
+var selectedSpaces = [];
 
-// on load: if Other checkbox is unchecked then disable #otherSpace
-if ($(otherCheckbox).is(':checked')) {
-  console.log('checkbox is checked');
-  $('#otherSpaceText').prop('disabled', false);
-} else if ($(otherCheckbox).not(':checked')) {
-  console.log('checkbox is not checked');
-  $('#otherSpaceText').prop('disabled', true);
+// if any checkboxes are checked
+if ($('#spaces input:checkbox:checked').length) {
+  // if Other checkbox is checked
+  if ($(otherCheckbox).is(':checked')) {
+    // enable the text box
+    $(copyOtherSpacesFrom).prop('disabled', false);
+  } else {
+    // disable the text box
+    $(copyOtherSpacesFrom).prop('disabled', true);
+  }
+} else {
+  $('#prefixing').hide();
+}
+
+
+$(copyOtherSpacesFrom).keyup(function() {
+  otherSpacesTo.text(this.value);
+});
+
+function matchSpaces(array, value, box) {
+  for (var i=0; i < array.length; i++) {
+    if (value == array[i].value){
+      // if you are checking the box
+      if ($(box).is(':checked')) {
+        // add the value to the list of selected spaces
+        selectedSpaces.push(array[i].value);
+      } else {
+        // assuming
+        var index = $.inArray(value, selectedSpaces);
+        // remove the value from the list of selected spaces
+        if(index != -1) {
+          selectedSpaces.splice(index, 1);
+          // update text
+
+        }
+      }
+    }
+  }
 }
 
 // when a checkbox is clicked on
 $('#spaces input[type=checkbox]').click(function() {
-  if ($(otherCheckbox).is(':checked')) {
-    console.log('checkbox is checked');
-    $(otherSpaceText).prop('disabled', false);
-  } else if ($(otherCheckbox).not(':checked')) {
-    console.log('checkbox is not checked');
-    $(otherSpaceText).prop('disabled', true);
+  var clickedCheckbox = this;
+  var clickedValue = this.value;
+
+  // if space is Other
+  if (clickedValue == 'Other') {
+    if ($(otherCheckbox).is(':checked')) {
+      $(copyOtherSpacesFrom).prop('disabled', false);
+    } else if ($(otherCheckbox).not(':checked')) {
+      $(copyOtherSpacesFrom).prop('disabled', true);
+      $(otherSpacesTo).text('');
+    }
+  } else {
+    if ($('body').hasClass('onlineUsage')) {
+      matchSpaces(onlineChoices,clickedValue,clickedCheckbox)
+    }
+    if ($('body').hasClass('offlineUsage')) {
+      matchSpaces(offlineChoices,clickedValue,clickedCheckbox)
+    }
   }
 
-  // if ($(otherCheckbox).is(':checked')) {
-  //   console.log('test');
-  //   $('#otherSpaceText').prop('disabled', false);
-  // }
-// if it's Other
-//   if (this.value == 'Other') {
-//
-// //  and if the box is already checked
-//     if ($(otherCheckbox).is(':checked')) {
-//       console.log('test');
-// //    uncheck the box
-//       // $(otherCheckbox).prop('checked',false);
-// //    disable #otherSpace
-// //    remove #otherSpace text from the preview
-//
-//     } else {
-//       $(otherCheckbox).prop('checked',true);
-//       //    check the box
-//       //    enable #otherSpace
-//       //    add #otherSpace text to the preview
-//     }
-// // else
-// //  if the box is already checked
-// //    uncheck the box
-// //    remove the value text from the preview
-// //  else
-// //    check the box
-// //    add the value text to the preview
+// else
+//  if the box is already checked
+//    uncheck the box
+//    remove the value text from the preview
+//  else
+//    check the box
+//    add the value text to the preview
 // }
 });
-
-// $('#spaces input[type=checkbox]').click(function() {
-//   console.log(this.value);
-//   if (this.value == 'Other') {
-//     if ($('input[type=checkbox][value=\'Other\']').prop('checked',true)) {
-//       console.log(this + 'already checked');
-//     } else {
-//       copyOtherSpacesFrom.prop('disabled', true);
-//     }
-//     copyOtherSpacesFrom.prop('disabled', false);
-//     $(copyOtherSpacesFrom).keyup(function() {
-//       copyOtherSpacesTo.text(prefixSpaces + this.value);
-//     });
-//     copyOtherSpacesTo.text($(prefixSpaces + 'input:checked').val())
-//   }
-// });
