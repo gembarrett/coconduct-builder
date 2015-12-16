f552b931ef0fbcd4a857820b53debb6ebbe28ab7
gembarrett <gem@gembarrett.com>
2015-12-16 15:25:26 -0500

    tidied up the popover styling a bit and fixed issue with marginalised group text being lost

--- src/scripts/template.js
+++ src/scripts/template.js
@@ -25,8 +25,10 @@ function getCodeOfConduct() {
   } else {
     eventType = 'on or offline community';
   }
+
+  var additionalConcerns = document.getElementById('otherAdditional').value;
   var shortVersion = 'Short version: ' + communityName + ' is dedicated to a harassment-free conference experience for everyone. Our anti-harassment policy can be found at: ' + codeOfConductUrl;
   var mediumVersion = 'Medium version: ' + communityName + ' is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age or religion ' + additionalConcerns + '. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any ' + eventType + ' venue, including talks. Conference participants violating these rules may be sanctioned or expelled from the ' + eventType + ' [without a refund] at the discretion of the conference organizers. Our anti-harassment policy can be found at: ' + codeOfConductUrl;
 
-  document.getElementById("copyableCoC").value = shortVersion;
+  document.getElementById("copyableCoC").value = shortVersion + '\n\n' + mediumVersion;
 }
