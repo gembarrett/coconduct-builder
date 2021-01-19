d7504be807793d518150414801b1b802c8605088
Gem Barrett <gembarrett@users.noreply.github.com>
2021-01-18 20:36:10 -0500

    removing old files

--- src/scripts/examples.js
+++
@@ -1,7 +0,0 @@
-var examplesEl = [$('#yesAdditionalC'), $('#noAdditionalC'), $('#circumstances .output #additionalC'), $('#otherAdditionalC'), 'We will also investigate reports of: '];
-
-addAdditionalOptions(examplesEl[0], examplesEl[1], examplesEl[2], examplesEl[3], examplesEl[4]);
-
-var notExamplesEl = [$('#yesAdditionalNC'), $('#noAdditionalNC'), $('#circumstancesExc .output #additionalNC'), $('#otherAdditionalNC'), 'In addition, we will not deal with reports of '];
-
-addAdditionalOptions(notExamplesEl[0], notExamplesEl[1], notExamplesEl[2], notExamplesEl[3], notExamplesEl[4]);
--- src/scripts/people.js
+++
@@ -1,3 +0,0 @@
-var peepsEl = [$('#yesAdditional'), $('#noAdditional'), $('#people .output #additional'), $('#otherAdditional'), 'We will also support the following groups of people in whatever way is needed: '];
-
-addAdditionalOptions(peepsEl[0], peepsEl[1], peepsEl[2], peepsEl[3], peepsEl[4]);
