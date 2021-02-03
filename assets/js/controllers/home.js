d374186dfb4f045eedd185a0da311dc244387d3e
Gem Barrett <gembarrett@users.noreply.github.com>
2021-02-02 19:26:24 -0500

    reset RoS content

--- assets/js/controllers/home.js
+++ assets/js/controllers/home.js
@@ -2,9 +2,7 @@ controllers.homePage = function(data, params){
   var homeContent = templates.intro();
   homeContent += '<div class="qContainer contain">';
   homeContent += templates.homeSection(ros[0].what);
-  homeContent += templates.homeSection(ros[0].who);
   homeContent += templates.homeSection(ros[0].how);
-  homeContent += templates.homeSection(ros[0].security);
   homeContent += templates.homeSection(ros[0].background);
   homeContent += templates.homeSection(ros[0].support);
   homeContent += '</div>';
--- content/rest-of-site.js
+++ content/rest-of-site.js
@@ -1,7 +1,7 @@
 var ros = [
   {
-    "head":"The free and simple tool that enables organisers and open-source maintainers to build better Code of Conduct policies.",
-    "subhead":"To get started, enter the name of your project:",
+    "head":"Two free and simple tools that enable organisers and open-source maintainers to build better Code of Conduct policies.",
+    "subhead":"To get started, enter the name of your event or project:",
     "what":[
       {
         "head":"What is this tool?",
@@ -16,65 +16,42 @@ var ros = [
         "links":[]
       }
     ],
-    "who":[
-      {
-        "head":"Who should use SOAP?",
-        "subhead":"Almost any project that needs or uses a Code of Conduct! In particular, this tool is geared towards helping open-source project maintainers, conference organisers and other community-based projects improve the safety of spaces by building a Code of Conduct that works for their specific situation.",
-        "list":"",
-        "more":["Research indicates nearly half of all CSOs don’t have a security policy. These organizations instead rely on informal guidelines and “trust” to manage their security. In today’s world, that’s not enough. Although such flexibility allows organizations to adapt quickly to new threats, it makes it much harder to ensure workers are operating under the same security standard. And new members are often left unprepared for even the most basic security incidents. SOAP provides a set of security rules that everyone can work with—all totally customizable to your organization’s specific needs.","So whether your organization needs to update an existing policy, create an entirely new one, or is just preparing to work with a security consultant, SOAP can help!"],
-        "links":[]
-      }
-    ],
     "how":[
       {
-        "head":"How does SOAP work?",
+        "head":"How does it work?",
         "subhead":"",
         "list":[
           {
             "type":"ol",
-            "content":["SOAP asks questions about your organizational security practices.","Your answers help SOAP to build a policy","Your policy is presented to you at the end of the process"]
+            "content":["step 1","step 2","step 3"]
           }
         ],
-        "more":["You can preview your policy at any point, and each question is accompanied by information to help you fully understand, research, and implement the associated practices. SOAP’s unique process focuses on key areas commonly included in policies, like devices or travel security.","Each section is preceded by a hypothetical—but common—security incident to help orient the questions within the context of a realistic threat."],
-        "links":[]
-      }
-    ],
-    "security":[
-      {
-        "head":"How secure and private is it?",
-        "subhead":"",
-        "list":[
-          {
-            "type":"ol",
-            "content":["Neither your answers nor any identifying data are stored or sent anywhere","SOAP relies on JavaScript to work, which may be relevant to your threat model","The code is open-source and available at <a href='https://github.com/gembarrett/soap' target='_blank' title='Go to the code repository on GitHub'>github.com/gembarrett/soap</a>"],
-          }
-        ],
-        "more":["SOAP is a web app that works solely in your browser: no cookies or server-side processing is involved in building your policy. No IP addresses, web logs, or any text entered into the answer boxes is stored or sent back to the SOAP servers. A <a href='assets/SOAP-alphaReport.pdf' target='_blank' title='Open PDF security report (SOAP alpha)'>report on SOAP's security</a> (alpha version - will be updated soon) was carried out by SubGraph as part of OTF's Red Team program, and verified that \"there are no meaningful attack vectors that could introduce substantial risk with the use of SOAP.\"","Like many websites, SOAP relies on JavaScript to achieve its functionality, so it must be enabled in your browser for SOAP to work (hint: if you’re reading this, it’s already enabled). If your threat model means you can’t turn on JavaScript, you can download the code (<a href='https://github.com/gembarrett/soap' target='_blank' title='Go to the code repository on GitHub'>github.com/gembarrett/soap</a>) to run the tool in your own secure environment. Questions? Concerns? Email <a href='mailto:feedback@usesoap.app?subject=Suggested%20change' title='Email your feedback on SOAP'>feedback@usesoap.app</a>"],
+        "more":[],
         "links":[]
       }
     ],
     "background":[
       {
-        "head":"What's behind the SOAP project?",
-        "subhead":"SOAP was researched, designed, and built by Gem Barrett, with funding from the Open Technology Fund. In developing SOAP, research was conducted to understand the frustrations faced and obstacles encountered by organizations when creating, implementing, and maintaining security policies.",
+        "head":"What's behind this project?",
+        "subhead":"This tool was originally conceived in 2016 as part of the Ford-Mozilla Open Web Fellowship, but was put on hold before it could be completed. In 2020, Gem Barrett revived the basic concept and launched a similar tool for building organisational security policies (https://usesoap.app). With the success of SOAP, Gem turned her focus back to the Code of Conduct Builder and it was finally launched in 2021.",
         "list":"",
-        "more":["The findings of this research and accompanying interviews are shared in the report “Securing Civil Society” (<a href='https://usesoap.app/assets/SecuringCivilSociety-report.pdf' target='_blank' title='Open \'Securing Civil Society\' PDF'>PDF currently available</a> in English, other versions coming soon). These findings, coupled with research into organizational security best practices, helped create the methodology that forms the basis of SOAP’s innovative process. For updates on SOAP's future development, follow <a href='https://twitter.com/@thatsoapapp' target='_blank' title='Get SOAP updates on Twitter'>@thatSOAPapp on Twitter</a>."],
+        "more":[],
         "links":[
           {
-            "name":"SAFETAG by Internews",
-            "url":"https://safetag.org"
+            "name":"Great CoC article",
+            "url":""
           },
           {
-            "name": "Umbrella by Security First",
-            "url": "https://secfirst.org/umbrella"
+            "name": "Another CoC article",
+            "url": ""
           },
           {
-            "name": "LevelUp by Engine Room",
-            "url":"https://www.level-up.cc"
+            "name": "And another CoC article",
+            "url":""
           },
           {
-            "name":"Security Planner by Citizen Lab",
-            "url":"https://securityplanner.org"
+            "name":"And yet another CoC article",
+            "url":""
           }
         ]
       }
@@ -82,11 +59,11 @@ var ros = [
     "support":[
       {
         "head":"Can I help support this project?",
-        "subhead":"Thanks for asking! It’s true: SOAP takes a lot of time (and coffee!) to maintain. So if you’re interested in giving back and supporting the project, here are a few options:",
+        "subhead":"Thanks for asking! It’s true: this tool takes a lot of time (and coffee!) to maintain. So if you’re interested in giving back and supporting the project, here are a few options:",
         "list":[
           {
             "type":"ul",
-            "content":["Donations to fund SOAP’s upkeep can be made at <a href='https://www.ko-fi.com/supportsoap' target='_blank' title='Donate via Ko-Fi.com'>ko-fi.com/supportsoap</a>","Fluent in a language other than English? Email <a href='mailto:feedback@usesoap.app?subject=Suggested%20change' title='Email your feedback'>feedback(at)usesoap.app</a> with the languages you can translate to and we'll let you know how you can help translate SOAP and make it more accessible to non-English speakers","SOAP is an open-source project. Contributions to the code (JavaScript, HTML or CSS) or documentation are always welcomed at <a href='https://github.com/gembarrett/soap' target='_blank' title='Go to the code repository at GitHub'>github.com/gembarrett/soap</a>"]
+            "content":["Donations to fund SOAP’s upkeep can be made at <a href='https://www.ko-fi.com/supportsoap' target='_blank' title='Donate via Ko-Fi.com'>ko-fi.com/supportsoap</a>","Fluent in a language other than English? Email <a href='mailto:feedback@usesoap.app?subject=Suggested%20change' title='Email your feedback'>feedback(at)codeofconduct.tools</a> with the languages you can translate to and we'll let you know how you can help translate the tool and make it more accessible to non-English speakers","SOAP is an open-source project. Contributions to the code (JavaScript, HTML or CSS) or documentation are always welcomed at <a href='https://github.com/gembarrett/coconduct-builder' target='_blank' title='Go to the code repository at GitHub'>github.com/gembarrett/coconduct-builder</a>"]
           }
         ],
         "more":["Just want to say hi or ask a question? Have an idea for how SOAP could be improved? Email us at: <a href='mailto:feedback@usesoap.app?subject=Suggested%20change' title='Email your feedback about SOAP'>feedback(at)usesoap.app</a>."]
