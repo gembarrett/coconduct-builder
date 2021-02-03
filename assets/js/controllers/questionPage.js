47a261e666ef302f7bb0d4f25ddaf73a245f2dd4
Gem Barrett <gembarrett@users.noreply.github.com>
2021-02-02 20:13:49 -0500

    removed build button in nav to force users to go via the home page

--- assets/js/controllers/questionPage.js
+++ assets/js/controllers/questionPage.js
@@ -1,5 +1,4 @@
 controllers.questionPage = function(data, params){
-  document.getElementById('bu').classList.add('active');
   // add class to body to change nav-start/scroll
   document.querySelector('body').classList.add('buildPage');
   var templateContext = [];
