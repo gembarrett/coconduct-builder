dec47e97e100ebb067da0c6105d39ad7a480d986
Gem Barrett <gembarrett@users.noreply.github.com>
2021-03-11 23:24:31 -0500

    updated styles, added links

--- assets/js/templates/homeSection.js
+++ assets/js/templates/homeSection.js
@@ -1,6 +1,7 @@
 templates.homeSection = function(data){
   var text = data[0];
   var list = "";
+  var links = "";
   // if there's a list
   if (text.list !== ""){
     list += `<`+text.list[0].type+`>`;
@@ -10,11 +11,18 @@ templates.homeSection = function(data){
     }
     list += `</`+text.list[0].type+`>`;
   }
+  if (text.links.length > 0){
+    var resources = text.links;
+    for (var r = 0; r < resources.length; r++){
+      links += `<a href="`+resources[r].url+`" target="_blank" class="btn btn-seco" title="Get more info about `+resources[r].name+`">`+resources[r].name+`</a>`;
+    }
+  }
+
   var content = `
   <div class="window">
     <h3>`+text.head+`</h3>
     <div class="homeSectionOverview"><p>`+text.subhead+`</p>`
-    +list+text.more+`</div>
-  </div>`;
+    +list+text.more+links+`</div></div>`;
+
   return content;
 };
