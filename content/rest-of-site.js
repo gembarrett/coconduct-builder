f62282b11a41af536a26557d872fff3994718195
Gem Barrett <gembarrett@users.noreply.github.com>
2021-04-07 22:53:18 -0400

    updated rest of site content

--- content/rest-of-site.js
+++ content/rest-of-site.js
@@ -1,28 +1,28 @@
 var ros = [
   {
-    "head":"So you want to build a Code of Conduct.",
+    "head":"So, you want to build a Code of Conduct.",
     "subhead":"Great! Let's start with the name of your event or project:",
     "buttons":[
       {
         "name":"Event",
-        "desc": "Use this tool to build a Code of Conduct for events like conferences, meetups and workshops."
+        "desc": "Use this tool to build a Code of Conduct for synchronous events like conferences, meetups and webinars."
       },
       {
         "name":"Project",
-        "desc": "Use this tool to build a Code of Conduct for projects like code repositories, mailing lists and forums."
+        "desc": "Use this tool to build a Code of Conduct for asynchronous projects like code repositories, mailing lists and forums."
       }
     ],
     "what":[
       {
         "head":"What",
-        "subhead":"This tool is a free, online Code of Conduct generator. It helps you to:",
+        "subhead":"This is a free, online Code of Conduct generator. It helps you to:",
         "list": [
           {
             "type": "ul",
             "content": ["Design a customized Code of Conduct for your event or project","Be better prepared for handling problems","Successfully implement and maintain your Code of Conduct"],
           }
         ],
-        "more": "At the end of the process you'll receive your Code of Conduct in a downloadable format. Along with that document you'll also get an appendix of supporting information for implementing your Code of Conduct.",
+        "more": "Some people have the time and skill to write their Code of Conduct from scratch. Others prefer to share and remix tried-and-tested policies written by others. This project sits between those two options by offering the ability to build a customised Code of Conduct step-by-step through a simple, informative question-and-answer format. <br /><br />This project began in 2015 as part of a Ford-Mozilla Open Web Fellowship, but it was paused before completion. In 2020, the idea was revived the idea as a <a href='https://usesoap.app'>tool for orgsec policies</a>. Building on that success and codebase, the Code of Conduct tools were developed and launched in 2021.",
         "links":[]
       }
     ],
@@ -36,23 +36,28 @@ var ros = [
             "content":["Answer questions about the behaviour expected from participants","Check the preview and edit your answers","Download your Code of Conduct"]
           }
         ],
-        "more":"The answers you provide will dictate which follow up questions are asked. From this, your Code of Conduct will be pieced together, a preview of which you can review at anytime. You can also go back to change your previous answers and see the updates reflected in the contents. At the end, you can download an HTML, Markdown or plaintext version of your Code of Conduct.",
+        "more":"The answers you provide will dictate which follow up questions are asked. From this, your Code of Conduct will be pieced together, a preview of which you can review at anytime. <br /><br />You can also go back to change your previous answers and see the updates reflected in the contents. At the end, you can download an HTML, Markdown or plaintext version of your Code of Conduct, along with an appendix of supporting information for implementing it.",
         "links":[]
       }
     ],
     "background":[
       {
-        "head":"Why",
-        "subhead":"Gem Barrett designed this tool in 2016 as part of their Ford-Mozilla Open Web Fellowship, but it was paused before completion. In 2020, they revived the idea in a <a href='https://usesoap.app'>tool for orgsec policies</a>. With that success, they turned their focus back to the Code of Conduct tool, which launched in 2021.",
-        "list":"",
-        "more":"This tool couldn't have been built if not for the hard work of those who have been fastidiously developing Code of Conduct resources for several years, and faced abuse in return for their efforts. Two resources in particular have been invaluable:",
+        "head":"Who",
+        "subhead":"This site is especially useful for those who are:",
+        "list":[
+          {
+            "type":"ol",
+            "content":["Building their first Code of Conduct", "Wanting to easily build a Code of Conduct from the ground up","Researching prior to writing their own Code of Conduct from scratch or remix"]
+          }
+        ],
+        "more":"In all, 16 Codes of Conduct were used in the creation of these tools and each relevant one is accredited at the end of the created document. This tool couldn't have been built if not for the hard work of those who have been fastidiously developing Code of Conduct resources for several years, in many cases facing abuse in return for their efforts. In particular, the actions of Adria Richards and the following two resources have been invaluable in laying the groundwork for this tool:",
         "links":[
           {
-            "name":"Contributor Covenant was created by Coraline Ada Ehmke and is released under the CC BY 4.0 License.",
+            "name":"Contributor Covenant by Coraline Ada Ehmke",
             "url":"https://www.contributor-covenant.org/"
           },
           {
-            "name": "Ashe Dryden's Code of Conduct 101",
+            "name": "Code of Conduct 101 by Ashe Dryden",
             "url": "https://www.ashedryden.com/blog/codes-of-conduct-101-faq"
           }
         ]
@@ -61,14 +66,14 @@ var ros = [
     "support":[
       {
         "head":"Support",
-        "subhead":"This site has been built by one person as a side project and as such is time-consuming to maintain. If you want to contribute ideas, report bugs or offer your skills, please head over to <a href='https://github.com/gembarrett/coconduct-builder' target='_blank' title='Go to the code repository at GitHub'>the GitHub repository</a>.",
+        "subhead":"This site has been built by one person as a side project and as such is time-consuming to maintain. If you want to contribute ideas, report bugs or offer your skills, please head over to <a href='https://github.com/gembarrett/coconduct-builder' target='_blank' title='Go to the code repository at GitHub'>the GitHub repository</a>. Contributions from those with the following skills are particularly encouraged:",
         "list":[
           {
             "type":"ul",
-            "content":[]
+            "content":["Able to translate from English to another language", "Content-editing and proofreading abilities", "Vanilla Javascript, HTML or CSS"]
           }
         ],
-        "more":"This is an open-source project which deliberately avoids the use of frameworks in order to be accessible to a wide range of frontend developers. If you want to help with the code (JavaScript, HTML or CSS) or documentation, please get in touch via <a href='https://github.com/gembarrett/coconduct-builder/issues'>the repository</a>.",
+        "more":"This is an open-source project which deliberately avoids the use of frameworks in order to be accessible to a wide range of frontend developers. If you want to help with the code but aren't sure where to start, please have a look at the  <a href='https://github.com/gembarrett/coconduct-builder/labels/good%20first%20issue'>the starter issues</a>.",
         "links":[]
       }
     ]
