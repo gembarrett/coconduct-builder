3211adc8886141861d268f5bbcb11947ed3b9b72
Gem Barrett <gembarrett@users.noreply.github.com>
2021-04-13 21:39:49 -0400

    updated section 2 content

--- content/section-2.js
+++ content/section-2.js
@@ -1,312 +1,312 @@
 var section2 = [
   {
     "isQuestion": false,
-    "id":"q17",
+    "id":"q42",
     "title": "Next section: Reporting",
     "paragraph":["In quis lacus imperdiet justo euismod vulputate eu a leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer blandit mauris vel est euismod, vitae rhoncus nisi tincidunt. Sed non sagittis orci, in varius sem.", "Phasellus et elit ac felis egestas semper. Nullam porttitor lorem sodales volutpat iaculis. Duis facilisis maximus lorem eget laoreet. Curabitur dignissim varius quam sed bibendum. Cras placerat, augue eget lobortis fermentum, neque mi fermentum nisl, non porta magna tellus at ipsum. Quisque fringilla aliquam erat a aliquam.", "Ut malesuada imperdiet lorem, sit amet semper velit commodo ac. Nam interdum gravida nunc, id consequat ex faucibus ultrices. Cras et sem eros. Proin erat sapien, lobortis vitae metus vel, viverra lacinia lorem."]
   },
   {
     "isQuestion": true,
-    "id":"q18",
-    "q":"How can people report a violation of the Code of Conduct?",
-    "required": false,
-    "policyEntry":"You can report a Code of Conduct violation through the following channels: [report-via]",
-    "answers":[
-      {
-        "type":"checkbox",
-        "answerText":"email",
-        "storeAs":"[report-via]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"checkbox",
-        "answerText":"in-person",
-        "storeAs":"[report-via]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"checkbox",
-        "answerText":"phone",
-        "storeAs":"[report-via]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"checkbox",
-        "answerText":"Twitter DM",
-        "storeAs":"[report-via]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"checkbox",
-        "editable": true,
-        "answerText":"another channel",
-        "storeAs":"[report-via]",
-        "excludes":[],
-        "policyEntry":"",
-      }
-    ],
-    "tips":[
-      {"meaning":"Make it easy for people to report violations. They may be upset or in distress and will need to be able to get help quickly and easily."},
-      {"more": [""]}
-    ]
-  },
-  {
-    "isQuestion": true,
-    "id":"q19",
-    "q":"How can people report a violation of the Code of Conduct?",
-    "required":false,
-    "policyEntry":"You can report a Code of Conduct violation through the following channels: [report-via]",
-    "answers":[
-      {
-        "type":"checkbox",
-        "answerText":"email",
-        "storeAs":"[report-via]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"checkbox",
-        "answerText":"phone",
-        "storeAs":"[report-via]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"checkbox",
-        "answerText":"Twitter DM",
-        "storeAs":"[report-via]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"checkbox",
-        "editable":true,
-        "answerText":"another channel",
-        "storeAs":"[report-via]",
-        "excludes":[],
-        "policyEntry":"",
-      }
-    ],
-    "tips":[
-      {"meaning":"Make it easy for people to report violations. They may be upset or in distress and will need to be able to get help quickly and easily."},
-      {"more": [""]}
-    ]
-  },
-  {
-    "isQuestion": true,
-    "id":"q20",
-    "q":"Can people make anonymous reports?",
-    "required": false,
-    "policyEntry":"",
-    "answers":[
-      {
-        "type":"radio",
-        "answerText":"Yes",
-        "storeAs":"",
-        "excludes":[],
-        "policyEntry":"> We can't follow up an anonymous report with you directly, but we will fully investigate it and take whatever action is necessary to prevent a recurrence.",
-      },
-      {
-        "type":"radio",
-        "answerText":"No",
-        "storeAs":"",
-        "excludes":[],
-        "policyEntry":"",
-      },
-    ],
-    "tips":[
-      {"meaning":"Some people may not feel able to report a violation if it means exposing their identity in the process, so consider providing an anonymous reporting mechanism."},
-      {"more": [""]}
-    ]
-  },
-  {
-    "isQuestion": true,
-    "id":"q21",
-    "q":"Do you have moderators for the online space?",
+    "id":"q43",
+    "spaces":["on"],
+    "position":["en",0],
+    "q":"Are the public conversations actively monitored for violations?",
     "required":false,
     "policyEntry":"",
     "answers":[
       {
         "type":"radio",
         "answerText":"Yes",
-        "storeAs":"",
+        "storeAs": "",
         "excludes":[],
-        "policyEntry":"Moderators will help to keep the conversations friendly and respectful in the event's online space.",
+        "policyEntry":"",
       },
       {
         "type":"radio",
         "answerText":"No",
-        "storeAs":"",
-        "excludes":[],
-        "policyEntry":"",
-      }
-    ],
-    "tips":[
-      {"meaning":"Having moderators in your event's online chat or forums will help with picking up on Code of Conduct violations that are occurring there."},
-      {"more": [""]}
-    ]
-  },
-  {
-    "isQuestion": true,
-    "id":"q22",
-    "q":"Who is responsible for handling reports?",
-    "required":false,
-    "policyEntry":"",
-    "answers":[
-      {
-        "type":"text",
-        "answerText":"Name",
-        "placeholder":"",
-        "storeAs":"[report-handlers]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"text",
-        "answerText":"Name",
-        "placeholder":"",
-        "storeAs":"[report-handlers]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"text",
-        "answerText":"Name",
-        "placeholder":"",
-        "storeAs":"[report-handlers]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"text",
-        "answerText":"Name",
-        "placeholder":"",
-        "storeAs":"[report-handlers]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"text",
-        "answerText":"Name",
-        "placeholder":"",
-        "storeAs":"[report-handlers]",
-        "excludes":[],
-        "policyEntry":"",
-      },
-      {
-        "type":"text",
-        "answerText":"Name",
-        "placeholder":"",
-        "storeAs":"[report-handlers]",
+        "storeAs": "",
         "excludes":[],
         "policyEntry":"",
       },
     ],
     "tips":[
       {"meaning":""},
-      {"more": [""]}
+      {"more":["lorem ipsum"]}
     ]
   },
   {
     "isQuestion": true,
-    "id":"q23",
-    "q":"How can people recognise who to approach for help?",
+    "id":"q44",
+    "spaces":["e","p"],
+    "position":["en",1],
+    "q":"Which of these are potential consequences for violations?",
     "required":false,
-    "policyEntry":"If you need help and you're not sure who to approach, here's a few things to look for. [help-identifiers]",
+    "policyEntry":"[name] community leaders are responsible for clarifying and enforcing our standards of acceptable behavior and will take appropriate and fair corrective action in response to any behavior that they deem inappropriate, threatening, offensive, or harmful. [name] reserves the right to make judgment calls about what is and isn't appropriate with regards to comments, commit messages and all other communications in the [name] [type] and to take any action it deems appropriate, up to and including:",
     "answers":[
       {
         "type":"checkbox",
-        "answerText":"Event logo on shirt",
-        "storeAs":"[help-identifiers]",
-        "excludes":[],
-        "policyEntry":"You can identify event staff by the event logo on their shirt.",
-      },
-      {
-        "type":"checkbox",
-        "answerText":"Public introduction",
-        "storeAs":"[help-identifiers]",
-        "excludes":[],
-        "policyEntry":"They will be introduced to event attendees in a public manner.",
-      },
-      {
-        "type":"checkbox",
-        "answerText":"Twitter DM",
-        "storeAs":"[help-identifiers]",
-        "excludes":[],
-        "policyEntry":"The event's Twitter account will have open direct messages, enabling anyone to send a private message to the team.",
-      },
-      {
-        "type":"checkbox",
-        "editable":true,
-        "answerText":"another identifier",
-        "storeAs":"[help-identifiers]",
+        "answerText":"general public reminder",
+        "storeAs":"[]",
         "excludes":[],
         "policyEntry":"",
-      }
-    ],
-    "tips":[
-      {"meaning":"Make it easy for people to recognise help."},
-      {"more": [""]}
-    ]
-  },
-  {
-    "isQuestion": true,
-    "id":"q24",
-    "q":"How can people recognise who to approach for help?",
-    "required":false,
-    "policyEntry":"If you need help and you're not sure who to approach, here's a few things to look for. [help-identifiers]",
-    "answers":[
-      {
-        "type":"checkbox",
-        "answerText":"Usernames",
-        "storeAs":"[help-identifiers]",
-        "excludes":[],
-        "policyEntry":"The usernames of people who can help will be shared with you.",
       },
       {
         "type":"checkbox",
-        "answerText":"Logo in avatar",
-        "storeAs":"[help-identifiers]",
-        "excludes":[],
-        "policyEntry":"Look for the [[name]] logo in avatars to find the people to approach for help.",
-      },
-      {
-        "type":"checkbox",
-        "editable":true,
-        "answerText":"another identifier",
-        "storeAs":"[help-identifiers]",
+        "answerText":"private warning",
+        "storeAs":"[]",
         "excludes":[],
         "policyEntry":"",
-      }
+      },
+      {
+        "type":"checkbox",
+        "answerText":"temp removal",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
+      {
+        "type":"checkbox",
+        "answerText":"permanent immediate expulsion",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
+      {
+        "type":"checkbox",
+        "answerText":"account deactivation",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
+      {
+        "type":"checkbox",
+        "answerText":"ban from future participation",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
+      {
+        "type":"checkbox",
+        "answerText":"public identification",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
+      {
+        "type":"checkbox",
+        "answerText":"public apology required",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
     ],
     "tips":[
-      {"meaning":"Make it easy for people to recognise help."},
-      {"more": [""]}
+      {"meaning":"Including a range of consequences allows freedom and flexibility to deal with violations of all severities."},
+      {"more":["lorem ipsum"]}
     ]
   },
   {
     "isQuestion": true,
-    "id":"q25",
-    "q":"Can you describe the process for handling reports?",
+    "id":"q45",
+    "spaces":["e","p"],
+    "position":["co",5],
+    "q":"Are some participants paying to access areas of this space?",
     "required":false,
-    "policyEntry":"[[name]] has developed the following process for handling reports of Code of Conduct violations: [process]",
+    "policyEntry":"",
     "answers":[
       {
-        "type":"textarea",
+        "type":"radio",
+        "answerText":"Yes",
+        "storeAs": "",
+        "excludes":[],
+        "policyEntry":"Those who are expelled from [name] for violating the Code of Conduct will not be issued a refund.",
+      },
+      {
+        "type":"radio",
+        "answerText":"No",
+        "storeAs": "",
+        "excludes":[],
+        "policyEntry":"",
+      },
+
+    ],
+    "tips":[
+      {"meaning":""},
+      {"more":["lorem ipsum"]}
+    ]
+  },
+  {
+    "isQuestion": true,
+    "id":"q46",
+    "spaces":["on"],
+    "position":["ch",2],
+    "q":"Are there circumstances under which the online chat could be shut down?",
+    "required":false,
+    "policyEntry":"",
+    "answers":[
+      {
+        "type":"radio",
+        "answerText":"No",
+        "storeAs": "",
+        "excludes":[],
+        "policyEntry":"",
+      },
+      {
+        "type":"text",
+        "answerText":"Yes, specifically when",
+        "placeholder":"",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"It may be deemed appropriate for [name] organisers to close down the chat facility, under circumstances which include:",
+      },
+
+    ],
+    "tips":[
+      {"meaning":"Chat functionality can be great for creating discussion alongside presentations, but with a lot of people it can get out of control if not monitored carefully for violations. So consider the eventualities where it may need to be shut down."},
+      {"more":["lorem ipsum"]}
+    ]
+  },
+  {
+    "isQuestion": true,
+    "id":"q47",
+    "spaces":["e","p"],
+    "position":["co",0],
+    "q":"Will you consider outside behaviour as grounds for exclusion from this space?",
+    "required":false,
+    "policyEntry":"",
+    "answers":[
+      {
+        "type":"radio",
+        "answerText":"Yes",
+        "storeAs": "",
+        "excludes":[],
+        "policyEntry":"This code of conduct applies to [name] spaces, but if you are being harassed by a participant of [name] [type] outside our spaces, we still want to know about it. We will take all good-faith reports of harassment by [name] members seriously. This includes harassment outside our spaces and harassment that took place at any point in time. [name] reserves the right to exclude people from our [type] based on their past behavior, including behavior outside [name] spaces and behavior towards people who are not in [name]. In order to protect volunteer admins from abuse and burnout, we reserve the right to reject any report we believe to have been made in bad faith. Reports intended to silence legitimate criticism may be deleted without response.",
+      },
+      {
+        "type":"radio",
+        "answerText":"No",
+        "storeAs": "",
+        "excludes":[],
+        "policyEntry":"",
+      },
+    ],
+    "tips":[
+      {"meaning":"Sometimes people have a habit of making shared spaces suck. If a participant has a reputation for harmful behaviour, consider how you can prevent them bringing that attitude into your community."},
+      {"more":["lorem ipsum"]}
+    ]
+  },
+  {
+    "isQuestion": true,
+    "id":"q48",
+    "spaces":["e","p"],
+    "position":["co",1],
+    "q":"Do you use the 3 strikes method?",
+    "required":false,
+    "policyEntry":"",
+    "answers":[
+      {
+        "type":"radio",
+        "answerText":"Yes",
+        "storeAs": "",
+        "excludes":[],
+        "policyEntry":"We use the 3 strikes method to enforce this Code of Conduct. The point of this section is not to find opportunities to punish people, but we do need a fair way to deal with people who make our community suck.",
+      },
+      {
+        "type":"radio",
+        "answerText":"No",
+        "storeAs": "",
+        "excludes":[],
+        "policyEntry":"",
+      },
+    ],
+    "tips":[
+      {"meaning":"Different spaces take different approaches to enforcement, but the 3 strikes method is popular as it creates a more transparent process and empowers community leaders by removing some of the uncertainty around how to deal with violations. Be prepared for participants to push those boundaries though, and allow for flexibility."},
+      {"more":["lorem ipsum"]}
+    ]
+  },
+  {
+    "isQuestion": true,
+    "id":"q49",
+    "spaces":["e","p"],
+    "position":["co",2],
+    "q":"Define the consequences for each strike:",
+    "required":false,
+    "policyEntry":"Please note that below are indications of probably course of action only, and [name] may amend these as it sees fit. You will be notified of each strike against the Code of Conduct. We will explain what section of the Code of Conduct has been violated, citing the particular behaviour that resulted in a strike. [answers]",
+    "answers":[
+      {
+        "type":"text",
         "answerText":"",
-        "placeholder":"This box will contain a process template so people can add to it",
-        "storeAs":"[process]",
+        "placeholder":"Example",
+        "storeAs":"[]",
         "excludes":[],
         "policyEntry":"",
-      }
+      },
+      {
+        "type":"text",
+        "answerText":"",
+        "placeholder":"Example",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
+      {
+        "type":"text",
+        "answerText":"",
+        "placeholder":"Example",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
     ],
     "tips":[
-      {"meaning":"Transparency is key to trust. Tell people how you're going to handle reports."},
-      {"more": [""]}
+      {"meaning":"Be clear about each strike: what is the community impact that will trigger this strike, and what will the consequence be. Add a fourth strike if you see fit."},
+      {"more":["lorem ipsum"]}
     ]
-  }
+  },
+  {
+    "isQuestion": true,
+    "id":"q50",
+    "spaces":["e","p"],
+    "position":["co",3],
+    "q":"Are there exceptions to the 3 strikes method?",
+    "required":false,
+    "policyEntry":"There are few exceptions to the 3 strikes method, specifically: ",
+    "answers":[
+      {
+        "type":"text",
+        "answerText":"",
+        "placeholder":"Example",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
+    ],
+    "tips":[
+      {"meaning":"Some violations should be dealt with in fewer than three strikes."},
+      {"more":["lorem ipsum"]}
+    ]
+  },
+  {
+    "isQuestion": true,
+    "id":"q51",
+    "spaces":["e","p"],
+    "position":["ha",0],
+    "q":"Where there is a clear harm being done, what will you do to ensure that those most impacted by a violation feel supported and are centred in the response?",
+    "required":false,
+    "policyEntry":"Where there is a clear harm being done, [name] is committed to ensuring that those most hurt by a violation are centred in our response.",
+    "answers":[
+      {
+        "type":"text",
+        "answerText":"",
+        "placeholder":"",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+      },
+    ],
+    "tips":[
+      {"meaning":"If someone has just been harmed by a violation, they are likely feeling upset and unsafe in your community. In addition to working with them on a resolution they’re satisfied by (if they want to), consider how to support them immediately, processing the report thoroughly and checking in afterwards."},
+      {"more":["lorem ipsum"]}
+    ]
+  },
 ]
